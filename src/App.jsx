9be9821db60fc37f8f25ea88261039cad992b33f
pouya963-ak <pakbari693@gmail.com>
2026-07-07 17:16:46 +0330

    initial react setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <h1>Reddit Mini</h1>
+      <p>vite + react is working.</p>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/api/redditApi.js
@@ -0,0 +1,27 @@
+const BASE_URL = 'https://www.reddit.com'
+
+export async function (getPostsBySubreddit = 'popular') {
+    const response = await fetch(`${BASE_URL}/t/${subreddit}.json`);
+
+    if (!response.ok) {
+        throw new Error('Failed to fetch Reddit posts');
+    }
+
+    const data = await response.json();
+
+    return data.data.children.map((post) => post.data);
+}
+
+export async function searchPosts(searchTerm) {
+    const response = await fetch(
+        `${BASE_URL}/searchPosts.json?q=${encodeURIComponent(searchTerm)}`
+    );
+
+    if (!response.ok) {
+        throw new Error('Failed to search Reddit posts');
+    }
+
+    const data = await response.json();
+
+    return data.data.chidren.map((post) => post.data);
+}
