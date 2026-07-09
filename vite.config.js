7e841c25d56d657f7fe664fcd0247267f38fa9c4
pouya963-ak <pakbari693@gmail.com>
2026-07-09 21:30:24 +0330

    deploy project to githup pages oyyyeee oy oy oy oyyyyeeee

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/reddit': {
-        target: 'https://www.reddit.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/reddit/, ''),
-        headers: {
-          'User-Agent': 'RedditMini/1.0',
-          Accept: 'application/json',
-        },
-      },
-    },
-  },
-});
+  base: '/Redditmini/',
+})
