9249c01d26f7185ade517fc2eaf3dbf9caa041e5
pouya963-ak <pakbari693@gmail.com>
2026-07-09 20:29:26 +0330

    add telegram update alert to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,97 +1,10 @@
 
-
-
-// import { useEffect, useState } from 'react';
-
-// import { getPostsBySubreddit } from './api/redditApi';
-// import './App.css';
-
-// function App() {
-//   const [posts, setPosts] = useState([]);
-//   const [status, setStatus] = useState('Loading Reddit posts...');
-
-//   useEffect(() => {
-//     getPostsBySubreddit('popular')
-//       .then((postsFromReddit) => {
-//         setPosts(postsFromReddit);
-//         setStatus('API test successful');
-//         console.log(postsFromReddit);
-//       })
-//       .catch((error) => {
-//         setStatus(`API test failed: ${error.message}`);
-//         console.error(error);
-//       });
-//   }, []);
-
-//   return (
-//     <main>
-//       <h1>Reddit Mini</h1>
-//       <p>{status}</p>
-//       <p>Fetched posts: {posts.length}</p>
-
-//       <ul>
-//         {posts.slice(0, 5).map((post) => (
-//           <li key={post.id}>{post.title}</li>
-//         ))}
-//       </ul>
-//     </main>
-//   );
-// }
-
-// export default App;
-
-
-
-// import { useEffect, useState } from 'react';
-// import { getPostsBySubreddit, searchPosts } from './api/redditApi';
-// import './App.css';
-
-// function App() {
-//   const [posts, setPosts] = useState([]);
-//   const [status, setStatus] = useState('Loading mock Reddit posts...');
-
-//   useEffect(() => {
-//     getPostsBySubreddit('popular')
-//       .then((postsFromApi) => {
-//         setPosts(postsFromApi);
-//         setStatus('Mock API test successful');
-//         console.log(postsFromApi);
-//       })
-//       .catch((error) => {
-//         setStatus(`Mock API test failed: ${error.message}`);
-//         console.error(error);
-//       });
-
-//     searchPosts('react').then((results) => {
-//       console.log('Search test:', results);
-//     });
-//   }, []);
-
-//   return (
-//     <main>
-//       <h1>Reddit Mini</h1>
-//       <p>{status}</p>
-//       <p>Fetched posts: {posts.length}</p>
-
-//       <ul>
-//         {posts.slice(0, 5).map((post) => (
-//           <li key={post.id}>
-//             r/{post.subreddit} — {post.title}
-//           </li>
-//         ))}
-//       </ul>
-//     </main>
-//   );
-// }
-
-// export default App;
-
-
-
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchPostsBySubreddit } from './store/redditSlice';
 import './App.css';
+
+import Header from '/Users/pouya/Documents/Career/Projects3/Redditmini/src/features/Header/Header.jsx'
 
 function App() {
   const dispatch = useDispatch();
@@ -110,19 +23,21 @@ function App() {
   }
 
   return (
-    <main>
-      <h1>Reddit Mini</h1>
-      <p>Redux fetch successful</p>
-      <p> Fethced posts: {posts.length} </p>
+    <div className="app">
+      <Header />
 
-      <ul>
-        {posts.slice(0, 5).map((post) => (
-          <li key={post.id}>
-            r/{post.subreddit} - {post.title}
-          </li>
-        ))}
-      </ul>
-    </main>
+      <main>
+        <p>Redux fetch successful</p>
+        <p> Fethced posts: {posts.length} </p>
+        <ul>
+          {posts.slice(0, 5).map((post) => (
+            <li key={post.id}>
+              r/{post.subreddit} - {post.title}
+            </li>
+          ))}
+        </ul>
+      </main>
+    </div>
   );
 }
 
