f79a71ad01b62902d5ed064e75f1c61059a68a07
pouya963-ak <pakbari693@gmail.com>
2026-07-09 22:23:36 +0330

    koskhare J and M ;)

--- src/features/Header/Header.jsx
+++ src/features/Header/Header.jsx
@@ -3,7 +3,7 @@ import './Header.css';
 function Header() {
     return (
         <header className='header'>
-            <h1 className="header__logo">Haji delam tang shode barash kharesho gaidan geee, oooooo oo ooooo oo ooooo oo, dobare del havaye , ba to boodan kardee</h1>
+            <h1 className="header__logo">Reddit Mini</h1>
         </header>
     );
 }
